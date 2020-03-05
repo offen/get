211e2e3ee516414577bb165261919056479d2394
Frederik Ring <frederik.ring@gmail.com>
2020-03-05 21:31:24 +0100

    download stable and latest channels from S3

--- main.go
+++ main.go
@@ -17,24 +17,34 @@ type releaseInfo struct {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	repo := os.Getenv("GITHUB_REPO")
-	endpoint := fmt.Sprintf("https://api.github.com/repos/%s/releases", repo)
+	var redirect string
+	if channel, ok := request.PathParameters["channel"]; ok {
+		switch channel {
+		case "latest", "stable":
+			redirect = fmt.Sprintf("https://offen.s3.eu-central-1.amazonaws.com/binaries/offen-%s.tar.gz", channel)
+		default:
+			return events.APIGatewayProxyResponse{}, fmt.Errorf("channel %s is not supported", channel)
+		}
+	} else {
+		repo := os.Getenv("GITHUB_REPO")
+		endpoint := fmt.Sprintf("https://api.github.com/repos/%s/releases", repo)
 
-	res, err := http.Get(endpoint)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		res, err := http.Get(endpoint)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+		defer res.Body.Close()
+
+		var data []releaseInfo
+		if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
+		redirect = data[0].Assets[0].BrowserDownloadURL
 	}
-	defer res.Body.Close()
-
-	var data []releaseInfo
-	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusFound,
 		Headers: map[string]string{
-			"Location": data[0].Assets[0].BrowserDownloadURL,
+			"Location": redirect,
 		},
 	}, nil
 }
